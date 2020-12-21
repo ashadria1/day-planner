00017f6519d268ee4ee5cbb1190d354b34ce826c
ashadria <ashadria@gmail.com>
2020-12-20 20:24:22 -0500

    adding javascript skeleton logic and variables

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ var workingHours = {
 var counter = 1;
 var timeId = "#time" + counter;
 var presentHour = moment().hour();
+var pastHour = moment().hour() - 1;
+var futureHour = moment().hour + 1;
 var timeString = $(timeId).text();  
+
   $('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+  function militaryTime(hourString) {
+    switch(hourString) {
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 PM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
+    }
+  }
   
