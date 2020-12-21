f2d027d24074029339b4fd3c578e4073b3e81dc3
ashadria <ashadria@gmail.com>
2020-12-20 20:09:14 -0500

    adding javascript and comments, polishing CSS

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-  
+var workingHours = {
+  "9 AM": "",
+  "10 AM": "",
+  "11 AM": "",
+  "12 PM": "",
+  "1 PM": "",
+  "2 PM": "",
+  "3 PM": "",
+  "4 PM": "",
+  "5 PM": "",
+};
+
+var counter = 1;
+var timeId = "#time" + counter;
+var presentHour = moment().hour();
+var timeString = $(timeId).text();  
   $('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
   
